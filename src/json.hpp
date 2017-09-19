8f169c9930d3dcba0e487c55e7a9139d6bea5dc9
Yoann Long <yoann.long@epitech.eu>
2017-09-19 20:14:52 +0200

    fix assignment within conditional expression on Visual Studio with W4

--- src/json.hpp
+++ src/json.hpp
@@ -3038,7 +3038,7 @@ class parser
         {
             case token_type::begin_object:
             {
-                if (keep and (not callback or ((keep = callback(depth++, parse_event_t::object_start, result)))))
+                if (keep and (not callback or ((keep = callback(depth++, parse_event_t::object_start, result)) != 0)))
                 {
                     // explicitly set result to object to cope with {}
                     result.m_type = value_t::object;
@@ -3134,7 +3134,7 @@ class parser
 
             case token_type::begin_array:
             {
-                if (keep and (not callback or ((keep = callback(depth++, parse_event_t::array_start, result)))))
+                if (keep and (not callback or ((keep = callback(depth++, parse_event_t::array_start, result)) != 0)))
                 {
                     // explicitly set result to object to cope with []
                     result.m_type = value_t::array;
